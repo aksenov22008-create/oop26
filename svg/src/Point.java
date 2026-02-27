ea553a48fb1a7dc4cd74c137ce4952bc66c42223
oop26 <akenov22008@gmail.com>
2026-02-27 11:46:27 +0100

    lab1 zad2

--- svg/src/Point.java
+++ svg/src/Point.java
@@ -1,7 +1,12 @@
+import java.util.Locale;
+
 public class Point {
     public float x;
     public float y;
     public String toString(){
         return "Point {x="+x+", y="+y+"}";
+    }
+    public String toSvg(){
+        return String.format(Locale.ENGLISH, "<circle r=\"45\" cx=\"%f\" cy=\"%f\" fill=\"red\"/>",x,y);
     }
 }
