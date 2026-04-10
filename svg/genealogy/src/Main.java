25c504da922fa5d38942818f1414c38ae2eeae8d
oop26 <akenov22008@gmail.com>
2026-04-10 11:06:48 +0200

    lab6 zad2

--- svg/genealogy/src/Main.java
+++ svg/genealogy/src/Main.java
@@ -1,11 +1,16 @@
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        Person person = Person.fromCsvLine("Marek Kowalski,15.05.1899,25.06.1957,,");
-        System.out.println(person);
+        try{
+            List<Person> people = Person.fromCsv("family.csv");
+            System.out.println(people);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
--- svg/genealogy/src/Person.java
+++ svg/genealogy/src/Person.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -71,6 +75,17 @@ public class Person implements Comparable{
             deathdate = LocalDate.parse(death,formatter);
         }
         return new Person(fname,lname,birthdate,deathdate);
+    }
+    public static List<Person> fromCsv(String path) throws IOException {
+        List<Person> people = new ArrayList<>();
+        BufferedReader file = new BufferedReader(new FileReader(path));
+        file.readLine();
+        String line;
+        while((line = file.readLine()) != null){
+            people.add(fromCsvLine(line));
+        }
+        file.close();
+        return people;
     }
     public String name(){
         return String.format("%s %s",firstName,lastName);
