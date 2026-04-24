23d8ed5ae4a592ff3775a73030cbc413f3e4a6d3
oop26 <akenov22008@gmail.com>
2026-04-24 11:52:06 +0200

    lab7 zad1 nie

--- svg/genealogy/src/Main.java
+++ svg/genealogy/src/Main.java
@@ -5,17 +5,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        try{
-            List<Person> people = Person.fromCsv("family.csv");
-            Person.toBinaryFile(people,"data.bin");
-            List<Person> people2 = Person.fromBinaryFile("data.bin");
-            System.out.println(people2);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+//        try{
+//            List<Person> people = Person.fromCsv("family.csv");
+//            Person.toBinaryFile(people,"data.bin");
+//            List<Person> people2 = Person.fromBinaryFile("data.bin");
+//            System.out.println(people2);
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        } catch (ClassNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
+        PluntUmlPunner.setJarPath("");
+        String umlData = "";
 
-
+        PluntUmlPunner.generator(umlData,"output","test");
     }
 }
---
+++ svg/genealogy/src/PluntUmlPunner.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class PluntUmlPunner {
+    private static String jarPath;
+    public static void setJarPath (String path){
+        jarPath=path;
+    }
+    public static void generator(String data,String outputPath,String filename){
+        File directory = new File(outputPath);
+        directory.mkdir();
+        File file = new File(outputPath+"/"+filename);
+        try{
+            FileWriter writer = new FileWriter(file);
+            writer.write(data);
+            writer.close();
+
+            ProcessBuilder builder = new ProcessBuilder("java","-jar",jarPath,file.getPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
