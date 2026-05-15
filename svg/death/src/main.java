f05dd226f4135c0c8891c7ad94c073dcfd970a96
oop26 <akenov22008@gmail.com>
2026-05-15 12:23:34 +0200

    lab9 zad4

--- svg/death/src/main.java
+++ svg/death/src/main.java
@@ -4,7 +4,10 @@ public class main {
     public static void main(String[] args){
         DeathCauseStatisticList staistics = DeathCauseStatisticList.fromCsv(Path.of("death/src/zgony.csv"));
         int age=70;
+        ICDCodeTabular icd = new ICDCodeTabularOptimizedForTime(Path.of("death/src/icd10.txt"));
         staistics.mostDeadlyDiseases(age,10).stream()
-                .forEach(stat -> System.out.println(stat.getCode()+"\t"+ stat.getAge(age).deathCount()));
+                .forEach(stat -> System.out.println(stat.getCode()+"\t"+ stat.getAge(age).deathCount()
+                                +" "+icd.getDescription(stat.getCode())
+                        ));
     }
 }
