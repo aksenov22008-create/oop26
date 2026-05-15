3ac755f016385f5f14bfc885dafea4000949c6c2
oop26 <akenov22008@gmail.com>
2026-05-15 11:53:51 +0200

    lab9 zad3

--- svg/death/src/main.java
+++ svg/death/src/main.java
@@ -1,9 +1,10 @@
+import java.nio.file.Path;
+
 public class main {
     public static void main(String[] args){
-        DeathCauseStatistic statistic = DeathCauseStatistic.fromCsvLine("A04.7          ,758,-,-,-,-,-,1,-,1,3,5,9,12,30,58,64,94,161,192,95,33");
-        System.out.println("KOD ICD-10:"+statistic.getCode());
-
-        DeathCauseStatistic.AgeBracketDeaths abd = statistic.getAge(77);
-        System.out.println(abd);
+        DeathCauseStatisticList staistics = DeathCauseStatisticList.fromCsv(Path.of("death/src/zgony.csv"));
+        int age=70;
+        staistics.mostDeadlyDiseases(age,10).stream()
+                .forEach(stat -> System.out.println(stat.getCode()+"\t"+ stat.getAge(age).deathCount()));
     }
 }
