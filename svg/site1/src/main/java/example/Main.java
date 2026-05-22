31e26882b4b7582e4e7176f710b39f61e8a772da
oop26 <akenov22008@gmail.com>
2026-05-22 11:22:06 +0200

    lab10 zad2

--- svg/site1/src/main/java/example/Main.java
+++ svg/site1/src/main/java/example/Main.java
@@ -12,6 +12,7 @@ public class Main {
         DaabaseConnection db = DaabaseConnection.getInstance();
         try {
             db.connect("site1/users.db");
+            insert("Jan","Nowak1234");
             selection();
             db.disconnect();
         } catch (SQLException ex) {
@@ -29,7 +30,23 @@ public class Main {
             int id = rs.getInt("id");
             String name =rs.getString("username");
             String path = rs.getString("password");
-            System.out.println("ID:"+id +"NAME:"+name+"HASLO:"+path);
+            System.out.println("ID:"+id +" NAME:"+name+" HASLO:"+path);
+        }
+    }
+    public static void insert(String name ,String password) throws SQLException {
+        Connection connection = DaabaseConnection.getInstance().getConnection();
+        String query = "INSERT INTO account (username, password) VALUES(?,?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(query, PreparedStatement.RETURN_GENERATED_KEYS);
+
+        preparedStatement.setString(1,name);
+        preparedStatement.setString(2,password);
+        preparedStatement.execute();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        while (resultSet.next()){
+            int id = resultSet.getInt(1);
+            System.out.println("Create ID: "+id);
         }
     }
 }
