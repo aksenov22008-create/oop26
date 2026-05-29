45260d3f1165ad83456aace68ece74b376f96b4d
oop26 <akenov22008@gmail.com>
2026-05-29 12:24:52 +0200

    lab11 zad 2d

--- svg/shop/src/test/java/SongTest.java
+++ svg/shop/src/test/java/SongTest.java
@@ -1,12 +1,18 @@
+import com.sun.jdi.connect.Connector;
 import database.DatabaseConnection;
 import music.Song;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.sql.SQLException;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class SongTest {
     @Test
@@ -25,5 +31,19 @@ public class SongTest {
         Optional<Song> song = Song.Persistence.read(68);
         assertTrue(song.isEmpty());
     }
+    private static Stream<Arguments> args(){
+        return Stream.of(arguments(40,"The Beatles","Help!",138),
+                arguments(41,"The Beach Boys","California Girls",165),
+                arguments(42,"The Temptations","Ain't Too Proud to Beg",154));
+    }
+    @ParameterizedTest
+    @MethodSource("args")
+    public void testReadMany(int id ,String artist,String title ,int length) throws SQLException {
+        DatabaseConnection.connect("songs.db","");
+        Optional<Song> song =Song.Persistence.read(id);
+        Song expectedSong = new Song(artist,title,length);
 
+        assertTrue(song.isPresent());
+        assertEquals(expectedSong,song.get());
+    }
 }
